66e922fd0b3562c22ead6390bc537cee094be618
superaxelink <superaxelink@hotmail.com>
2024-02-08 19:59:29 -0600

    Prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -53,3 +53,13 @@ def evaluate_models(X_train, y_train, X_test, y_test, models, params):
   
   except Exception as e:
     raise CustomException(e, sys)
+  
+def load_object(file_path):
+  print("BISTEKES")
+  print(file_path)
+  try:
+    with open(file_path,"rb") as file_obj:
+      return dill.load(file_obj)
+
+  except Exception as e:
+    raise CustomException(e, sys)
