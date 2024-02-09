478a98535dfe6719ad226075617a97787ebe06bc
superaxelink <superaxelink@hotmail.com>
2024-02-08 18:26:23 -0600

    Hyperparameter training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -15,7 +15,7 @@ from src.utils import save_object
 
 @dataclass
 class DataTransformationConfig:
-  preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pk1')
+  preprocessor_obj_file_path = os.path.join('artifacts', 'preprocessor.pkl')
 
 class DataTransformation:
   def __init__(self):
